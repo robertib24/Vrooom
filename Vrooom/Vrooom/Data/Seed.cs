e0b583ec6dea35d3bfb38f3e4bebe5755c4f2928
Robert <rb.iulian24@gmail.com>
2025-05-12 21:17:48 +0300

    + data seeding

---
+++ Vrooom/Vrooom/Data/Seed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Vrooom.Models.Enum;
+
+namespace Vrooom.Data
+{
+    public class Seed
+    {
+
+        public static async Task InitializeRoles(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+                foreach (var roleName in Enum.GetNames(typeof(Roles)))
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                        await roleManager.CreateAsync(new IdentityRole<int>(roleName));
+                }
+            }
+        }
+    }
+}
